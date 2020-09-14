7ffd71ceb800c053cfdad145ffb20b0d556d59ce
Alejandro <pa.sarmiento10@uniandes.edu.co>
2020-09-13 23:24:55 -0500

    feat: Add unit tests

---
+++ server/test/unit/IPLocationRepository.test.js
@@ -0,0 +1,94 @@
+const ipLocationRepository = require('../../repository/IPLocationRepository')
+const IPLocation = require('../../businessEntity/IPLocation')
+const IPLocationModel = require('../../model/IPLocationModel')
+const faker = require('faker')
+const sinon = require('sinon')
+const redisClient = require('../../utils/redis')
+
+/**
+ *
+ * Unit test IPLocation Repository
+ */
+
+describe('IP location service ', () => {
+  beforeEach(() => {
+    this.redisGet = sinon.stub(redisClient, 'get')
+    this.redisSet = sinon.stub(redisClient, 'set')
+    this.redisDel = sinon.stub(redisClient, 'del')
+    this.redisExpire = sinon.stub(redisClient, 'expire')
+    this.ipLocationModel = sinon.stub(IPLocationModel, 'find')
+    this.ipLocationModelSave = sinon.stub(IPLocationModel.prototype, 'save').yields(null, {})
+    this.ipLocationModelUpdate = sinon.stub(IPLocationModel, 'updateOne')
+  })
+  afterEach(() => {
+    this.redisGet.restore()
+    this.redisSet.restore()
+    this.redisDel.restore()
+    this.redisExpire.restore()
+    this.ipLocationModel.restore()
+    this.ipLocationModelSave.restore()
+    this.ipLocationModelUpdate.restore()
+  })
+  it('findIPLocation from Redis', async () => {
+    const ip = faker.internet.ip()
+    const ipLocation = new IPLocation(ip, 'United States', 'US', false)
+    ipLocation.country.trm = 'EUR'
+    ipLocation.country.trm.addRate('USD', 1.18441)
+    const redisResp = `{"_ip":"${ip}","_isBlackListed":false,"_country":{"_name":"United States","_code":"US","_trm":{"_base":"EUR","_rate":[{"_name":"USD","_value":1.18441}]}}}`
+    this.redisGet.resolves(redisResp)
+    const data = await ipLocationRepository.findIPLocation(ip)
+    expect(data).toEqual(ipLocation)
+  })
+
+  it('findIPLocation from DB', async () => {
+    const ip = faker.internet.ip()
+    const ipLocation = new IPLocation(ip, 'United States', 'US', false)
+    ipLocation.country.trm = 'EUR'
+    ipLocation.country.trm.addRate('USD', 1.6464)
+    this.redisGet.resolves(undefined)
+    const respModel = [
+      {
+        isblackListed: false,
+        _id: '5f5edb4a02ac320ef340ea8e',
+        ip: ip,
+        country: { name: 'United States', code: 'US', trm: { base: 'EUR', rates: [{ rate: 'USD', value: 1.6464 }] } },
+        __v: 0
+      }
+    ]
+    this.ipLocationModel.resolves(respModel)
+    this.redisSet.resolves({})
+    this.redisExpire.resolves({})
+    const data = await ipLocationRepository.findIPLocation(ip)
+    expect(data).toEqual(ipLocation)
+  })
+
+  it('saveIPLocation success', async () => {
+    const ip = faker.internet.ip()
+    const ipLocation = new IPLocation(ip, 'United States', 'US', false)
+    ipLocation.country.trm = 'EUR'
+    ipLocation.country.trm.addRate('USD', 1.47654)
+    this.redisSet.resolves({})
+    this.redisExpire.resolves({})
+    const trm = { base: 'EUR', rates: [{ rate: 'USD', value: 1.47654 }] }
+    const data = await ipLocationRepository.saveIPLocation(ip, false, 'United States', 'US', trm)
+    expect(data).toEqual(ipLocation)
+  })
+
+  it('updateIpLocation Ip has not been created', async () => {
+    const ip = faker.internet.ip()
+    const ipParams = {
+      ip,
+      isblackListed: true
+    }
+    const ipLocation = new IPLocation(ip, 'United States', 'US', true)
+    ipLocation.country.trm = 'EUR'
+    ipLocation.country.trm.addRate('USD', 1.6282)
+
+    this.ipLocationModel.resolves([])
+    try {
+      await ipLocationRepository.updateIpLocation(ipParams)
+    } catch (error) {
+      expect(error.message).toEqual('Ip has not been created')
+    }
+  })
+})
---
+++ server/test/unit/IPLocationService.test.js
@@ -0,0 +1,69 @@
+const ipLocationService = require('../../service/IPLocationService')
+const ipLocationRepository = require('../../repository/IPLocationRepository')
+const IPLocation = require('../../businessEntity/IPLocation')
+const faker = require('faker')
+const sinon = require('sinon')
+const axios = require('axios')
+/**
+ *
+ * Unit test IPLocation Service
+ */
+
+describe('IP location service ', () => {
+  beforeEach(() => {
+    this.findIPLocation = sinon.stub(ipLocationRepository, 'findIPLocation')
+    this.saveIPLocation = sinon.stub(ipLocationRepository, 'saveIPLocation')
+    this.updateIpLocation = sinon.stub(ipLocationRepository, 'updateIpLocation')
+    this.axiosStub = sinon.stub(axios, 'get')
+  })
+  afterEach(() => {
+    this.findIPLocation.restore()
+    this.saveIPLocation.restore()
+    this.updateIpLocation.restore()
+    this.axiosStub.restore()
+  })
+  it('Get ip Location from DB', async () => {
+    const ip = faker.internet.ip()
+    const ipLocation = new IPLocation(ip, 'United States', 'US', false)
+    ipLocation.country.trm = 'EUR'
+    ipLocation.country.trm.addRate('USD', 1.18441)
+    this.findIPLocation.resolves(ipLocation)
+    const data = await ipLocationService.getIpLocation(ip)
+    expect(data).toEqual(ipLocation)
+  })
+
+  it('Get ip Location from API', async () => {
+    const ip = faker.internet.ip()
+    const ipLocation = new IPLocation(ip, 'United States', 'US', false)
+    ipLocation.country.trm = 'EUR'
+    ipLocation.country.trm.addRate('USD', 1.18441)
+    this.findIPLocation.resolves(undefined)
+    this.axiosStub.resolves({ data: { currencies: [{ code: 'US' }], success: true, rates: { US: 7643, COP: 354.3 } } })
+    this.saveIPLocation.resolves(ipLocation)
+    const data = await ipLocationService.getIpLocation(ip)
+    expect(data).toEqual(ipLocation)
+  })
+
+  it('Register API in black list when Ip exists in DB', async () => {
+    const ip = faker.internet.ip()
+    const ipLocation = new IPLocation(ip, 'United States', 'US', false)
+    ipLocation.country.trm = 'EUR'
+    ipLocation.country.trm.addRate('USD', 1.18441)
+    this.updateIpLocation.resolves(ipLocation)
+    const data = await ipLocationService.registerIpBlackList(ip)
+    expect(this.saveIPLocation.called)
+    expect(data).toEqual({ _message: 'Success' })
+  })
+
+  it('Register API in black list when Ip does not exists in DB', async () => {
+    const ip = faker.internet.ip()
+    const ipLocation = new IPLocation(ip, 'United States', 'US', false)
+    ipLocation.country.trm = 'EUR'
+    ipLocation.country.trm.addRate('USD', 1.18441)
+    this.updateIpLocation.throws(new Error('Ip has not been created'))
+    this.saveIPLocation.resolves(ipLocation)
+    const data = await ipLocationService.registerIpBlackList(ip)
+    expect(this.saveIPLocation.called)
+    expect(data).toEqual({ _message: 'Success' })
+  })
+})
